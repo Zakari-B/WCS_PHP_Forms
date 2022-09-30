dfa73ffdac1d40e6250aaff9c3fd487ba42130d8
Zakari-B <99882585+Zakari-B@users.noreply.github.com>
2022-09-30 10:04:21 +0200

    Updated for PHP Forms exercise 2

--- form.php
+++ form.php
@@ -1,18 +1,18 @@
 <form action="thanks.php" method="post">
     <div>
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="user_name">
+        <input type="text" id="nom" name="user_name" placeholder="John Doe" required>
     </div>
     <div>
         <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" name="user_email">
+        <input type="email" id="courriel" name="user_email" placeholder="Johndoe1436@gmail.com" required>
     </div>
     <div>
         <label for="telephone">Téléphone :</label>
-        <input id="telephone" name="user_phone">
+        <input type="tel" id="telephone" name="user_phone" pattern="[0-9]{10}" placeholder="0238145749" required>
     </div>
     <div>
-        <select name="user_subject" id="sujet">Sujet :
+        <select name="user_subject" id="sujet" required>Sujet :
             <option value="contact">Contact</option>
             <option value="suggestion">Suggestion</option>
             <option value="bug">Bug</option>
@@ -20,7 +20,7 @@
     </div>
     <div>
         <label for="message">Message :</label>
-        <textarea id="message" name="user_message"></textarea>
+        <textarea id="message" name="user_message" placeholder="Hello, I'm contacting you about ..." required></textarea>
     </div>
     <div class="button">
         <button type="submit">Envoyer votre message</button>
--- thanks.php
+++ thanks.php
@@ -1,7 +1,49 @@
 <?php
 
-echo 'Merci ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['user_subject'] . '. <br />';
+$usernameErr = $emailErr = $phoneErr = $messageErr = "";
+$username = $email = $phone = $message = "";
 
-echo 'Un de nos conseiller vous contactera soit à l’adresse ' . $_POST['user_email']  . ' ou par téléphone au ' . $_POST['user_phone'] . ' dans les plus brefs délais pour traiter votre demande :<br />';
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
-echo $_POST['user_message'] . '<br />';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["user_name"])) {
+    $usernameErr = "Name is required";
+  } else {
+    $username = test_input($_POST["user_name"]);
+  }
+
+  if (empty($_POST["user_email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["user_email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+
+  if (empty($_POST["user_phone"])) {
+    $phoneErr = "Phone is required";
+  } else {
+    $phone = test_input($_POST["user_phone"]);
+  }
+
+  if (empty($_POST["user_message"])) {
+    $messageErr = "Message is required";
+  } else {
+    $message = test_input($_POST["user_message"]);
+  }
+}
+
+if ($usernameErr === "" && $emailErr === "" && $phoneErr === "" && $messageErr === "") {
+  echo 'Merci ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['user_subject'] . '. <br />';
+
+  echo 'Un de nos conseiller vous contactera soit à l’adresse ' . $_POST['user_email']  . ' ou par téléphone au ' . $_POST['user_phone'] . ' dans les plus brefs délais pour traiter votre demande :<br />';
+
+  echo $_POST['user_message'] . '<br />';
+}
